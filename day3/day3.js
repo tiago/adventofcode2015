517d447e386edf7cef302d8496033f5aee3b5e36
Tiago Oliveira <tiago.co@gmail.com>
2015-12-12 01:45:43 +0000

    Create `Map` constructor function

--- day3/day3.js
+++ day3/day3.js
@@ -1,11 +1,27 @@
 #! /usr/bin/env node
 'use strict';
 
+function Map() {
+    var map = Object.create(null);
+
+    this.visit = function visit(position) {
+        var mapKey = position.x + 'x' + position.y;
+        if (!(mapKey in map)) {
+            map[mapKey] = true;
+        }
+    };
+
+    this.getVisitedPositions = function getVisitedPositions() {
+        return Object.keys(map);
+    };
+}
+
 function Santa(map) {
     var position = {
         x: 0,
         y: 0
     };
+
     this.move = function move(direction) {
         switch (direction) {
         case '<':
@@ -21,28 +37,25 @@ function Santa(map) {
             position.y++;
             break;
         }
-        var positionKey = position.x + 'x' + position.y;
-        if (!(positionKey in map)) {
-            map[positionKey] = true;
-            this.houseCount++;
-        }
+        map.visit(position);
     };
+
     this.move(null); // initial position
 }
 
 function countHouses(directions) {
-    var map = Object.create(null);
+    var map = new Map();
     var santa = new Santa(map);
 
     for (var i = 0; i < directions.length; i++) {
         santa.move(directions[i]);
     }
 
-    return Object.keys(map).length;
+    return map.getVisitedPositions().length;
 }
 
 function countHousesWithRobot(directions) {
-    var map = Object.create(null);
+    var map = new Map();
     var human = new Santa(map);
     var robot = new Santa(map);
 
@@ -51,7 +64,7 @@ function countHousesWithRobot(directions) {
         santa.move(directions[i]);
     }
 
-    return Object.keys(map).length;
+    return map.getVisitedPositions().length;
 }
 
 module.exports = {
