0deb7397205d7d1f6d8b5635b1b362927c70d335
Tiago Oliveira <tiago.co@gmail.com>
2015-12-13 20:27:54 +0000

    Minor fix

--- day4/day4.spec.js
+++ day4/day4.spec.js
@@ -4,10 +4,9 @@ var expect = require('chai').expect;
 var day4 = require('./day4');
 
 xdescribe('day4', function () {
-
     describe('#mineAdventCoin()', function () {
         it('should find the numberic answer of the secret key', function () {
-            this.timeout(20000);
+            this.timeout(30000);
             expect(day4.mineAdventCoin('abcdef')).to.equal(609043);
             expect(day4.mineAdventCoin('pqrstuv')).to.equal(1048970);
         });
